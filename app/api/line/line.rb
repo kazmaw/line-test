a51fc2946926f0cb4e5e2e5c4d2675bbd69e5902
kazmaw <z3.am.au.p@gmail.com>
2021-08-29 11:32:13 +0900

    add api

---
+++ app/api/line/line.rb
@@ -0,0 +1,23 @@
+module Line
+  class API < Grape::API
+    version 'v1', using: :header, vendor: 'line'
+    format :json
+    prefix :api
+    # helpers do
+    #   def current_user
+    #     @current_user ||= User.authorize!(env)
+    #   end
+
+    #   def authenticate!
+    #     error!('401 Unauthorized', 401) unless current_user
+    #   end
+    # end
+
+    resource :messaging do
+      desc "user test"
+      post do
+        User.all
+      end
+    end
+  end
+end
